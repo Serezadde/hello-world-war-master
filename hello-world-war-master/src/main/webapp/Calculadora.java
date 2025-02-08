9b0e59dd9d50d959c63d9f5683189cd419e7989e
Serezzade <pokemonespartano@gmail.com>
2025-02-08 00:41:42 +0000

    ultimo commit

---
+++ hello-world-war-master/src/main/webapp/Calculadora.java
@@ -0,0 +1,21 @@
+/**
+ * Clase que representa una calculadora simple.
+ */
+public class Calculadora {
+
+    /**
+     * Suma dos números enteros.
+     *
+     * @param a El primer número.
+     * @param b El segundo número.
+     * @return La suma de a y b.
+     */
+    public int sumar(int a, int b) {
+        return a + b;
+    }
+
+
+    public int restar(int a, int b) {
+        return a - b;
+    }
+}
